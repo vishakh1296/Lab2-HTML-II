dc0c3aaf58116b27558ab9694da6ebbb25b6715c
vishakh kompella <vkompell@confederationcollege.ca>
2020-01-17 16:02:46 -0500

    Create english.js

---
+++ Dhruti/english.js
@@ -0,0 +1,87 @@
+//https://www.codewars.com/kata/58dbdccee5ee8fa2f9000058/solutions/javascript
+
+function spEng(sentence){
+
+ let english = "english"
+
+ let first = 0
+
+ let second = 0
+
+  let lower = sentence.toLowerCase()
+
+    for(let i = 0; i < sentence.length; i++){
+
+        if(lower[i] == "e"){
+
+          first = i
+
+          console.log("first: ", lower[i], first, sentence.length)
+
+          }
+
+          if(lower[i] == "h"){
+
+          second = i + 1
+
+          console.log("second: ", sentence[i], second)
+
+          }
+
+          if(lower.substring(first, second) == english){
+
+            return true;
+
+            }
+
+
+
+    }
+
+    return false;
+
+
+}
+
+function spEng(sentence){
+
+ let english = "english"
+
+ let first = 0
+
+ let second = 0
+
+  let lower = sentence.toLowerCase()
+
+    for(let i = 0; i < sentence.length; i++){
+
+        if(lower[i] == "e"){
+
+          first = i
+
+          console.log("first: ", lower[i], first, sentence.length)
+
+          }
+
+          if(lower[i] == "h"){
+
+          second = i + 1
+
+          console.log("second: ", sentence[i], second)
+
+          }
+
+          if(lower.substring(first, second) == english){
+
+            return true;
+
+            }
+
+
+
+    }
+
+    return false;
+
+
+}
